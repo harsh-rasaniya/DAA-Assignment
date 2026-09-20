3022a75e702e3f090bbc3fc29493010ba1880d25
harsh-rasaniya <harshrasaniya393@gmail.com>
2026-09-21 00:25:36 +0530

    Add files via upload

---
+++ 33 Search in Rotated Sorted Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0,right = nums.length-1,mid=left+(right-left)/2;
+        while(left<=right){
+            mid = left + (right-left)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[left]<=nums[mid]){
+                // Left Part is sorted
+                if(nums[left]<=target && nums[mid]>target){
+                    right = mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }else{
+                // Right Part is Sorted
+                if(nums[right]>=target && nums[mid]<target){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
